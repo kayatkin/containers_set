51993d42631d49cfa6e19f8806db6429bd0fa7e3
Viktor Kayatkin <kayatkin@mail.ru>
2024-01-06 20:09:28 +0700

    initial commit

---
+++ src/js/Team.js
@@ -0,0 +1,23 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error("Этот персонаж уже в команде.");
+    }
+
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => {
+      this.members.add(character);
+    });
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
